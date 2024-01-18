5574faf15e2b9fd59c202b79e190d4c270bcf016
ashraf62069mustafa <116838514+ashraf62069mustafa@users.noreply.github.com>
2024-01-18 18:06:49 +0530

    today my work mustafa

--- src/App.js
+++ src/App.js
@@ -10,13 +10,22 @@ import NotWorkingUserPage from './pages/notWorkingUser';
 import Footer from './common/footer/Footer';
 import Dashboardpage from './pages/dashboard';
 import AlltransportationReportsPage from './pages/alltransportationReports';
+import BalanceTransferPage from './pages/balanceTransfer';
+import BalanceReturnRequestPage from './pages/balanceReturnRequest';
+import PaymentRequestViewPage from './pages/paymentRequestView';
+import PaymentRequestPage from './pages/PaymentRequest';
+import PendingDisputPage from './pages/pendingDisput';
+import SolveDisputePage from './pages/solveDispute';
+import DistributerIncomePage from './pages/distributerIncome';
+import RetailerIncomePage from './pages/retailerIncome';
+import MyIncomePage from './pages/myIncome';
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-      <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Dashboardpage />} />
         <Route path="member-list/distributor" element={<DistributerPage />} />
         <Route path="create-user/distributor" element={<CreateUserDistributerPage />} />
@@ -24,9 +33,18 @@ function App() {
         <Route path="create-user/retailer" element={<CreateUserRetailerPage />} />
         <Route path="suspended-users" element={<SuspendedUserPage />} />
         <Route path="not-working-users" element={<NotWorkingUserPage />} />
-        <Route path="all-transactions-report" element={< AlltransportationReportsPage/>} />
+        <Route path="all-transactions-report" element={< AlltransportationReportsPage />} />
+        <Route path="balance-trasnfer" element={< BalanceTransferPage />} />
+        <Route path="balance-return-request" element={<BalanceReturnRequestPage />} />
+        <Route path="payment-request-view" element={<PaymentRequestViewPage />} />
+        <Route path="payment-request" element={<PaymentRequestPage />} />
+        <Route path="pending-dispute" element={<PendingDisputPage />} />
+        <Route path="solve-dispute" element={<SolveDisputePage />} />
+        <Route path="user-income/distributor" element={<DistributerIncomePage />} />
+        <Route path="income/user-income/retailer" element={<RetailerIncomePage />} />
+        {/* <Route path="income/my-income" element={<MyIncomePage />} /> */}
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
